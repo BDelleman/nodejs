3f3a1e57c882953097a2f0c587554d1db5153dc9
BDelleman <48587856+BDelleman@users.noreply.github.com>
2019-03-15 11:20:09 +0100

    Add files via upload

---
+++ Spotifyding.js
@@ -0,0 +1,4 @@
+var artistEnricherApiLib = module.exports;
+
+artistEnricherApiLib.token = 'BQBhuFdacQVAODhUdFeLKGCvncLV5QE-vb1i58UkwuT3JwDD1a-ayRskkLkX0eY3kcaLrHHq0ia2ggQ4lIUCS5ijt6JkvbvsQKPB_ZhvbgsEbCBjlky_6oJYxRJDvyxHv0LOUJLYZ9RFC2l2_qMT';
+artistEnricherApiLib.sportify = 'http://api.spotify.com/v1';
---
+++ artist-enricher-api-lib.js
@@ -0,0 +1,5 @@
+var artistEnricherApiLib = module.exports;
+
+// please go to https://developer.spotify.com/web-api/console/get-search-item/ and try out the API; request an OAuth token; copy and paste the value of the token in the next line
+artistEnricherApiLib.token = 'BBQCTO7wQmjLqimF8hQZyD5zbk2dDD3QIG5OO8wD5i4QG13B809-fxDk1qhL8I4U3W5Hq8_pJXEAoUsRsuUeoSGI89IpmK_4YGzV0Nu2qbTSfh3xJhv24JCIgsEcIv6n6Kd4ZI_FeH64S_zYFBECI';
+artistEnricherApiLib.spotifyAPI = 'https://api.spotify.com/v1';
